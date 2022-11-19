0a27893c1e3f0db4bd405c2d1f595c4b0b400a86
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2022-11-19 08:38:40 -0300

    adicionando função de aumentar e diminuir a fonte por javascript

---
+++ js/fonte.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    let fonte = 14
+    $('#aumenta').click(function() {
+        if(fonte < 25)
+        {
+            $('body').css({'font-size': ++fonte + 'px'})
+        }
+    })
+    $('#diminui').click(function() {
+        if(fonte > 14)
+        {
+            $('body').css({'font-size': --fonte + 'px'})
+        }
+    })
+})
